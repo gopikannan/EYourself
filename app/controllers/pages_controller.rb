91993017b06106712f3455ec41c79e711350023d
Pradeep <praad.kumar@gmail.com>
2011-11-13 13:59:29 -0500

    first commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,13 @@
+class PagesController < ApplicationController
+  def home
+          @poll = Poll.new
+          @poll_items = Poll.all
+  end
+
+  def login
+          auth = request.env["omniauth.auth"]
+          user = User.find_by_fb_id(auth["uid"]) || User.create_with_omniauth(auth)
+          session[:user_id] = user.id
+          redirect_to root_path
+  end
+end
